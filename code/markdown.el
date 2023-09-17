208d0a6b5d3c6b7d7b4a31df23e09215dc46a3f6
Adi Bozzhanov <adi.bozzhanov@accessia.com>
2023-09-17 01:37:53 +0100

    Update markdown x2

--- code/markdown.el
+++ code/markdown.el
@@ -2,4 +2,4 @@
   :ensure t
   :commands (markdown-mode gfm-mode)
   :mode ("README\\.md\\'" . gfm-mode)
-  :init (setq markdown-command "multimarkdown"))
+  :init (setq markdown-command "/usr/local/bin/multimarkdown"))
