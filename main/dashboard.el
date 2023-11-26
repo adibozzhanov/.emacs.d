38d2b6c76829c260fcde791a1982223065412160
Adi <bozzhanov.at@gmail.com>
2023-11-26 01:02:13 +0000

    All the icons support + dashboard cutification

--- main/dashboard.el
+++ main/dashboard.el
@@ -4,18 +4,20 @@
   :config
   (dashboard-setup-startup-hook))
 
+(setq dashboard-set-heading-icons t)
+(setq dashboard-icon-type 'all-the-icons)
+(setq dashboard-set-file-icons t)
 (setq dashboard-banner-logo-title "ADI'S EMACS")
 (setq dashboard-startup-banner 2)
+(setq dashboard-center-content t)
 (setq dashboard-show-shortcuts t)
 (setq dashboard-set-navigator t)
 (setq dashboard-init-info "You are safe here. Nobody can hurt you here. Just keep coding and you will be fine.")
 (setq dashboard-set-footer nil)
-
-
-(setq dashboard-items '((recents  . 10)
+(setq dashboard-items '((recents  . 5)
+			(bookmarks . 5)
                         (projects . 5)))
 
-
-(setq dashboard-icon-type 'all-the-icons)
-(dashboard-modify-heading-icons '((recents . "file-text")
-                                  (projects . "book")))
+(dashboard-modify-heading-icons '((recents . "clock")
+				  (bookmarks . "bookmark")
+                                  (projects . "repo")))
