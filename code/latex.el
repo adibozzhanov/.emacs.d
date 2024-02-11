aaca73a57bdacdaaa87c88ccdbf3ebff2f5a7a5f
Adi <bozzhanov.at@gmail.com>
2024-02-11 16:59:15 +0000

    Added smarted intellisense

--- code/latex.el
+++ code/latex.el
@@ -1,5 +1,14 @@
 (use-package tex
-  :ensure auctex)
+  :pin gnu
+  :ensure auctex
+  :mode ("\\.tex\\$" . latex-mode)
+  :custom
+  (TeX-auto-save t)
+  (TeX-parse-self t)
+  (TeX-master nil))
 
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
+(use-package company-auctex
+  :pin melpa
+  :after tex
+  :init
+  (company-auctex-init))
